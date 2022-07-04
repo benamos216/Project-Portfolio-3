b249601707a107cba1c00c949405ddd144e4b4b9
benamos216 <benamos216@gmail.com>
2022-07-04 19:01:28 +0000

    set up start up page for Battleship game

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def start_up():
+    """
+    Loads up main start menu for the user.
+    Giving them the option to start a game or not.
+    Also outlines the game rules.
+    """
+    print("Welcome to Battleships!\n")
+    print("A turn based game, where user and computer take turns to guess")
+    print("where each others Battleships are.")
+    print("The winner is whoever sinks all their oppenents Battleships first!\n")
+    print("Are you ready to Battle!?")
+
+
+
+start_up()
