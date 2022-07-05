60eb0a0d597ebb5001ebf688222646339b49f7bf
benamos216 <benamos216@gmail.com>
2022-07-05 20:56:22 +0000

    Set up class to create game board, set up main function to run overall program

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-def start_up():
+def load_game():
     """
     Loads up main start menu for the user.
     Giving them the option to start a game or not.
@@ -12,8 +12,25 @@ def start_up():
     print("A turn based game, where user and computer take turns to guess")
     print("where each others Battleships are.")
     print("The winner is whoever sinks all their oppenents Battleships first!\n")
-    print("Are you ready to Battle!?")
+    print("Are you ready to Battle!? Y/N\n")
+    
+def start_game():
+    x = input()
+    if x == "Y":
+        create_game()
+    elif x == "N":
+        exit()
+    else:
+        print("Please enter Y/N\n")
+        start_game()
 
 
+def create_game():
+    print("Game starting up....\n")
 
-start_up()
+
+def main():
+    load_game()
+    start_game()
+
+main()
