5c08081a66b94a8642a6e68d7f6c2bf8cd0743d0
benamos216 <benamos216@gmail.com>
2022-07-10 13:46:32 +0000

    scraped original code, rewrote code for playboard, creating ships and location. Working on generating computer turn.

--- run.py
+++ run.py
@@ -3,98 +3,89 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from random import randint
 
-class Board:
+#Board for holding ship locations
+HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
+# Board for displaying hits and misses
+GUESS_BOARD = [[" "] * 8 for i in range(8)]
 
-    def __init__(self, size, num_ships, name, type):
-        self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
-        self.num_ships = num_ships
-        self.name = name
-        self.type = type
-        self.guesses = []
-        self.ships = []
-    
-    def print(self):
-        for row in self.board:
-            print(" ".join(row))
+COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
 
-    def guess(self, x, y):
-        self.guesses.append((x, y))
-        self.board[x][y] = "X"
+COMPUTER_PLAY_BOARD = [[" "] * 8 for i in range(8)]
 
-        if (x, y) in self.ships:
-            self.board[x][y] = "*"
-            return "Hit"
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
+#computer create 5 ships
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = get_ship_location()
+        board[ship_row][ship_column] = "X"
+
+def get_ship_location():
+    row = input("Enter the row of the ship: ").upper()
+    while row not in "12345678":
+        print('Not an appropriate choice, please select a valid row')
+        row = input("Enter the row of the ship: ").upper()
+    column = input("Enter the column of the ship: ").upper()
+    while column not in "ABCDEFGH":
+        print('Not an appropriate choice, please select a valid column')
+        column = input("Enter the column of the ship: ").upper()
+    return int(row) - 1, letters_to_numbers[column]
+
+def computer_turn():
+
+
+#check if all ships are hit
+def count_hit_ships(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == "X":
+                count += 1
+    return count
+
+def play_game():
+    create_ships(HIDDEN_BOARD)
+    create_ships(COMPUTER_BOARD)
+    print('Guess a battleship location\n')
+    print('Player Board\n')
+    print_board(GUESS_BOARD)
+    print('\nComputer Board\n')
+    print_board(COMPUTER_PLAY_BOARD)
+    while count_hit_ships(GUESS_BOARD) < 6, count_hit_ships(COMPUTER_PLAY_BOARD) < 6:
+        print_board(GUESS_BOARD)
+        print_board(COMPUTER_PLAY_BOARD)
+        row, column = get_ship_location()
+        row, column = computer_turn()
+        if GUESS_BOARD[row][column] == "-":
+            print("You guessed that one already.")
+        elif HIDDEN_BOARD[row][column] == "X":
+            print("Hit")
+            GUESS_BOARD[row][column] = "X" 
         else:
-            return "Miss"
-    
-    def add_ship(self, x, y, type = "Computer"):
-        if len(self.ships) >= self.num_ships:
-            print("Error")
-        else:
-            self.ships.append((x,y))
-            if self.type == "Player":
-                self.board[x][y] = "@"
+            print("MISS!")
+            GUESS_BOARD[row][column] = "-"      
+        if count_hit_ships(GUESS_BOARD) == 5:
+            print("You win!")
+            GUESS_BOARD[row][column] = "X"
+            break
+            
 
-
-
-def load_game():
-    """
-    Loads up main start menu for the user.
-    Giving them the option to start a game or not.
-    Also outlines the game rules.
-    """
-    print("Welcome to Battleships!\n")
-    print("A turn based game, where user and computer take turns to guess")
-    print("where each others Battleships are.")
-    print("The winner is whoever sinks all their oppenents Battleships first!\n")
-    print("Are you ready to Battle!? Y/N\n")
-    
-def start_game():
-    x = input()
-    if x == "Y":
-        map_size()
-    elif x == "N":
-        exit()
-    else:
-        print("Please enter Y/N\n")
-
-
-def map_size():
-    print("Please choose your map size, 6, 8 or 10!\n")
-    size = input()
-    if size == "6":
-        print("Game size 6")
-        return size
-    elif size == "8":
-        print("Game size 8")
-        return size
-    elif size == "10":
-        print("Game size 10")
-        return size
-    else:
-        print("Please choose your grid size!\n")
-        map_size()
-
-
-def main():
-
-    load_game()
-    start_game()
-
-    size = map_size()
-    num_ships = 4
-    scores["Computer"] = 0
-    scores["Player"] = 0
-
-    computer_board = Board(size, num_ships, "Computer", type="Computer")
-    player_board = Board(size, num_ships, "Player", type = "Player")
-
-    for _ in range(num_ships):
-        populate_board(player_board)
-        populate_board(computer_board)
-
-    play_game(computer_board, player_board)
-
-
-main()
+play_game()
