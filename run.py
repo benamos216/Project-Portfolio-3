8bde2cb306395bae7e71da93d2634200e446cbf6
benamos216 <benamos216@gmail.com>
2022-07-10 20:28:36 +0000

    Added comments to each main function to allow others what it is to do.

--- run.py
+++ run.py
@@ -4,16 +4,20 @@
 from random import randint
 from random import random
 
-#Board for holding ship locations
+#Player Board for holding ship locations
 HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
-# Board for displaying hits and misses
+#Player Board for displaying hits and misses
 GUESS_BOARD = [[" "] * 8 for i in range(8)]
-
+#Computer Board for holding ship locations
 COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
-
+#Computer Board for displaying hits and misses
 COMPUTER_PLAY_BOARD = [[" "] * 8 for i in range(8)]
 
 def print_board(board):
+    """
+    Generates an 8 x 8 grid to display the game as it goes along
+    A - H letters used for the columns, 1 - 8 used for the rows
+    """
     print("  A B C D E F G H")
     print("  +-+-+-+-+-+-+-+")
     row_number = 1
@@ -31,8 +35,11 @@ letters_to_numbers = {
     'G': 6,
     'H': 7
 }
-#computer create 5 ships
+
 def create_ships(board):
+    """
+    Randomly generates 5 ships on both players boards
+    """
     for ship in range(5):
         ship_row, ship_column = randint(0,7), randint(0,7)
         while board[ship_row][ship_column] == "X":
@@ -40,6 +47,10 @@ def create_ships(board):
         board[ship_row][ship_column] = "X"
 
 def get_ship_location():
+    """
+    Player input for their guesses. Checks that each input is valid
+    and informs the player if they are made an incorrect input.
+    """
     row = input("Enter the row of the ship: ").upper()
     while row not in "12345678":
         print('Not an appropriate choice, please select a valid row')
@@ -51,13 +62,19 @@ def get_ship_location():
     return int(row) - 1, letters_to_numbers[column]
 
 def computer_turn():
+    """
+    Generates the computer's turn, by randomly picking a row
+    and column value within the parameters of the grid.
+    """
     row = randint(1,8)
     column = randint(1,8)
     return int(row) - 1, int(column) - 1
 
 
-#check if all ships are hit
 def count_hit_ships(board):
+    """
+    Checks if all the ships on the board have been hit
+    """
     count = 0
     for row in board:
         for column in row:
